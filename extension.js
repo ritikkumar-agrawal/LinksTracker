e762f2e80d8cc5fa6d1e66463b198c360b34074c
Ritik <ritikagrawal3268@gmail.com>
2023-03-07 11:29:51 +0530

    2nd commit

--- extension.js
+++ extension.js
@@ -2,21 +2,40 @@ let myLinks = [];
 const textBox = document.querySelector("#text-box");
 const saveInputBtn = document.querySelector("#save-input-btn");
 const uoList = document.querySelector("#uo-list");
+const deletBtn = document.querySelector("#delete-btn");
+const saveTabBtn = document.querySelector("#save-tab-btn");
+const linksFromLocalStorage = JSON.parse( localStorage.getItem("myLinks") );
 
+if(linksFromLocalStorage){
+  myLinks = linksFromLocalStorage ;
+  render(myLinks);
+}
+saveTabBtn.addEventListener("click", function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    myLinks.push(tabs[0].url);
+    localStorage.setItem("myLinks", JSON.stringify(myLinks));
+    render(myLinks);
+  })
+})
+deletBtn.addEventListener("dblclick", function(){
+  localStorage.clear();
+  myLinks = [];
+  render(myLinks); 
+})
 saveInputBtn.addEventListener("click", function () {
   if (textBox.value != "") {
     myLinks.push(textBox.value);
   }
   // console.log(myLinks);
   textBox.value = "";
-  renderlinks();
+  localStorage.setItem("myLinks", JSON.stringify(myLinks));
+  render(myLinks);
 });
-
-function renderlinks() {
+function render(linksArray) {
   let listitems = "";
-  for (let i = 0; i < myLinks.length; i++) {
+  for (let i = 0; i < linksArray.length; i++) {
     // uoList.innerHTML += "<li>" + myLinks[i] + "</li>";
-    listitems += `<li><a target='_blank' href='${myLinks[i]} '>${myLinks[i]}</a></li>`;
+    listitems += `<li><a target='_blank' href='${linksArray[i]} '>${linksArray[i]}</a></li>`;
   }
   uoList.innerHTML = listitems;
   // document.getElementById("text-box").value = "";
